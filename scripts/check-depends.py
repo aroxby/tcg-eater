785286a896d0fc98862fb24e3ee80b08f81e2c41
Andy Roxby <aroxby@users.noreply.github.com>
2025-09-02 14:13:23 -0400

    Create script to check circuit dependencies

---
+++ scripts/check-depends.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import json
+from pathlib import Path
+import sys
+
+
+def main():
+    found_components = set()
+    missing_components = set()
+
+    json_files = Path('.').glob('**/*.json')
+    for json_file in json_files:
+        with open(json_file) as fp:
+            circuit = json.load(fp)
+            found_components.add(circuit["save_id"])
+            missing_components.update(circuit["dependencies"])
+
+    unmet = missing_components - found_components
+    if(len(unmet) > 0):
+        print("Missing components:", unmet)
+        return 1
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
